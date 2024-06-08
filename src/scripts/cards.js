c28ff71409b83563c1db87499e588ed7a923c509
MatheusFonseca849 <matheusfonseca849@gmail.com>
2024-06-08 18:45:49 -0300

    style: initial stylization implemented

--- src/scripts/cards.js
+++ src/scripts/cards.js
@@ -1,108 +1,116 @@
-export function createCard(object){
-    
-    let li = document.createElement("li")
-    li.classList = "productCard"
-    li.title = object.name
+export function createCard(object) {
+  let li = document.createElement("li");
+  li.classList = "productCard";
+  li.title = object.name;
 
-    let imgContainer = document.createElement("div")
-    imgContainer.classList = "cardImgContainer"
+  let imgContainer = document.createElement("div");
+  imgContainer.classList = "cardImgContainer";
 
-    let img = document.createElement("img")
-    img.src = object.imgPath
-    img.alt = object.name
+  let img = document.createElement("img");
+  img.src = object.imgPath;
+  img.alt = object.name;
 
-    imgContainer.append(img)
+  imgContainer.append(img);
 
-    let cardInfo = document.createElement("div")
-    cardInfo.classList = "cardInfo"
+  let cardInfo = document.createElement("div");
+  cardInfo.classList = "cardInfo";
 
-    let productName = document.createElement("h3")
-    productName.innerHTML = object.name
-    productName.title = "Nome do produto"
+  let productName = document.createElement("h3");
+  productName.innerHTML = object.name;
+  productName.title = "Nome do produto";
 
-    let productPlatform = document.createElement("p")
-    productPlatform.title = "Plataforma/Fabricante do Produto"
+  let productPlatform = document.createElement("p");
+  productPlatform.title = "Plataforma/Fabricante do Produto";
 
-    if(object.category == "games"){
-        productPlatform.innerHTML = object.platform
-    }else{
-        productPlatform.innerHTML = object.manufacturer
-    }
+  if (object.category == "games") {
+    productPlatform.innerHTML = object.platform;
+  } else {
+    productPlatform.innerHTML = object.manufacturer;
+  }
 
-    let productPrice = document.createElement("span")
-    productPrice.innerHTML = `R$ ${object.price.toFixed(2).replace(".", ",")}`
-    productPrice.title = "Preço do Produto"
+  let productPrice = document.createElement("span");
+  productPrice.innerHTML = `R$ ${object.price.toFixed(2).replace(".", ",")}`;
+  productPrice.title = "Preço do Produto";
 
-    cardInfo.append(productName, productPlatform, productPrice)
+  cardInfo.append(productName, productPlatform, productPrice);
 
-    let cardBtnContainer = document.createElement("div")
-    cardBtnContainer.classList = "cardBtnContainer"
+  let cardBtnContainer = document.createElement("div");
+  cardBtnContainer.classList = "cardBtnContainer";
 
-    let addToCartBtn = document.createElement("button")
-    addToCartBtn.title = "Adicionar ao Carrinho"
-    addToCartBtn.innerHTML = "<i class='fa-solid fa-cart-plus'>"
+  let addToCartBtn = document.createElement("button");
+  addToCartBtn.classList = "btnDefault";
+  addToCartBtn.title = "Adicionar ao Carrinho";
+  addToCartBtn.innerHTML = "<i class='fa-solid fa-cart-plus'>";
 
-    let moreInfoBtn = document.createElement("button")
-    moreInfoBtn.title = "Mais Informações"
-    moreInfoBtn.innerHTML = "<i class='fa-solid fa-plus'></i>"
+  let moreInfoBtn = document.createElement("button");
+  moreInfoBtn.classList = "btnDefault";
+  moreInfoBtn.title = "Mais Informações";
+  moreInfoBtn.innerHTML = "<i class='fa-solid fa-plus'>";
 
-    let wishlistBtn = document.createElement("button")
-    wishlistBtn.title = "Adicionar à lista de desejos"
-    wishlistBtn.innerHTML = "<i class='fa-solid fa-heart'></i>"
+  let wishlistBtn = document.createElement("button");
+  wishlistBtn.classList = "btnDefault";
+  wishlistBtn.title = "Adicionar à lista de desejos";
+  wishlistBtn.innerHTML = "<i class='fa-solid fa-heart'>";
 
-    cardBtnContainer.append(addToCartBtn, moreInfoBtn, wishlistBtn)
+  cardBtnContainer.append(addToCartBtn, moreInfoBtn, wishlistBtn);
 
-    li.append(imgContainer, cardInfo, cardBtnContainer)
+  li.append(imgContainer, cardInfo, cardBtnContainer);
 
-    return li
-    
+  return li;
 }
 
-export function createPurchasedCard(object){
-    let li = document.createElement("li")
-    li.classList = "purchasedProduct"
-    li.title = object.name
+export function createPurchasedCard(object) {
+  let li = document.createElement("li");
+  li.classList = "purchasedProduct";
+  li.title = object.name;
 
-    let cardImgContainer = document.createElement("div")
-    cardImgContainer.classList = "cardImgContainer"
+  let cardImgContainer = document.createElement("div");
+  cardImgContainer.classList = "cardImgContainer";
 
-    let cardImg = document.createElement("img")
-    cardImg.src = object.imgPath
-    cardImg.alt = object.name
+  let cardImg = document.createElement("img");
+  cardImg.src = object.imgPath;
+  cardImg.alt = object.name;
 
-    cardImgContainer.append(cardImg)
+  cardImgContainer.append(cardImg);
 
-    let productInfo = document.createElement("div")
+  let div = document.createElement("div")
+  div.classList = "purchasedCardContainer"
 
-    let productName = document.createElement("h3")
-    productName.innerHTML = object.name
+  let productInfo = document.createElement("div");
+  productInfo.classList = "purchasedProductInfo"
 
-    let productPrice = document.createElement("span")
-    productPrice.innerHTML = `R$ ${object.price.toFixed(2).replace(".", ",")}`
+  let productName = document.createElement("h3");
+  productName.innerHTML = object.name;
 
-    productInfo.append(productName, productPrice)
+  let productPrice = document.createElement("span");
+  productPrice.innerHTML = `R$ ${object.price.toFixed(2).replace(".", ",")}`;
 
-    let counter = document.createElement("div")
-    counter.classList = "productCounter"
+  productInfo.append(productName, productPrice);
 
-    let counterBtnContainer = document.createElement("div")
-    counterBtnContainer.classList = "counterBtnContainer"
+  let counter = document.createElement("div");
+  counter.classList = "productCounter";
 
-    let counterPlusBtn = document.createElement("button")
-    counterPlusBtn.innerHTML = "+"
+  let counterBtnContainer = document.createElement("div");
+  counterBtnContainer.classList = "counterBtnContainer";
 
-    let counterMinusBtn = document.createElement("button")
-    counterMinusBtn.innerHTML = "-"
+  let counterPlusBtn = document.createElement("button");
+  counterPlusBtn.classList = "btnDefault counterBtn"
+  counterPlusBtn.innerHTML = "+";
 
-    counterBtnContainer.append(counterPlusBtn, counterMinusBtn)
+  let counterMinusBtn = document.createElement("button");
+  counterMinusBtn.classList = "btnDefault counterBtn"
+  counterMinusBtn.innerHTML = "-";
 
-    let counterDisplay = document.createElement("span")
-    counterDisplay.innerHTML = "1"
+  counterBtnContainer.append(counterPlusBtn, counterMinusBtn);
 
-    counter.append(counterBtnContainer, counterDisplay)
+  let counterDisplay = document.createElement("span");
+  counterDisplay.innerHTML = `Quantidade: 1`;
 
-    li.append(cardImgContainer, productInfo, counter)
+  counter.append(counterBtnContainer, counterDisplay);
 
-    return li
+  div.append(productInfo, counter)
 
+  li.append(cardImgContainer, div);
+
+  return li;
 }
