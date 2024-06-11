e321fc51b840c18260afc73f2aaaf5dce70d23fb
MatheusFonseca849 <matheusfonseca849@gmail.com>
2024-06-11 18:16:23 -0300

    feat: basic add to cart feature implemented

--- src/scripts/shoppingCart.js
+++ src/scripts/shoppingCart.js
@@ -1,30 +1,35 @@
 import { appendPurchasedCard } from "./index.js";
-import { shoppingCartArray } from "./database.js";
+import { shoppingCartArray, productDatabase } from "./database.js";
 
-let shoppingCart = document.getElementById("shoppingCart")
-let itemCountHtml = document.getElementById("itemCount")
-let totalPriceHtml = document.getElementById("totalPrice")
+let shoppingCart = document.getElementById("shoppingCart");
+let itemCountHtml = document.getElementById("itemCount");
+let totalPriceHtml = document.getElementById("totalPrice");
 
-export function showShoppingCart(){
-    shoppingCart.innerHTML = ""
-    let itemCount = 0
-    let totalPrice = 0
+export function showShoppingCart() {
+  shoppingCart.innerHTML = "";
+  let itemCount = 0;
+  let totalPrice = 0;
 
-    shoppingCartArray.forEach((product) => {
-        itemCount = itemCount + product.quantity
-        totalPrice = totalPrice + product.price * product.quantity
-        appendPurchasedCard(product, shoppingCart)
-    })
+  shoppingCartArray.forEach((product) => {
+    itemCount = itemCount + product.quantity;
+    totalPrice = totalPrice + product.price * product.quantity;
+    appendPurchasedCard(product, shoppingCart);
+  });
 
-    itemCountHtml.innerHTML = `Total de itens: ${itemCount}`
-    totalPriceHtml.innerHTML = `Valor Total: R$ ${totalPrice.toFixed(2).replace(".", ",")}`
+  itemCountHtml.innerHTML = `Total de itens: ${itemCount}`;
+  totalPriceHtml.innerHTML = `Valor Total: R$ ${totalPrice
+    .toFixed(2)
+    .replace(".", ",")}`;
 }
 
 export const addItem = (id) => {
-    let product = shoppingCartArray.filter(product => product.id == id)
-    console.log(product)
-    product.quantity = Number(product.quantity) + 1
-    // shoppingCartArray = [...shoppingCartArray, product]
-}
+  let product = productDatabase.find((product) => product.id == id);
 
-showShoppingCart()
+  console.log(product);
+  console.log(shoppingCartArray);
+
+  shoppingCartArray.push({ ...product, quantity: 1 });
+  showShoppingCart();
+};
+
+showShoppingCart();
